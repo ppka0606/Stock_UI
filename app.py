ce9ee4b7946811cc0e196b99ea70cc74b54e5c83
ppka0606 <ppka0606@gmail.com>
2020-05-02 23:29:53 +0800

    定义了check按钮的绑定触发事件

--- app.py
+++ app.py
@@ -11,10 +11,11 @@ stats = Stats()
 
 # 标题栏
 window.title("股票查询")
+window.resizable(0,0) #防止用户调整尺寸
 # window.geometry("500x400")
 
 
-# label_welcome = tk.Label(window, text = "欢迎使用股票信息查询工具", font=('Arial', 12), width = 30, height = 2)
+# label_welcome = tk.Label(window, text = "欢迎使用股票信息查询工具", font=('Arial', 12), width = 40, height = 2)
 # label_welcome.pack()
 
 # 导航栏
@@ -46,26 +47,31 @@ window.config(menu = menubar)
 label_author = tk.Label(window, text = "  @ppka0606")
 label_author.grid(row = 11, column = 7)
 
-label_version = tk.Label(window, text = " Version : 1.0")
+label_version = tk.Label(window, text = "          ")
 label_version.grid(row = 0, column = 0)
 
 # 输入序号栏
 var_entry = tk.StringVar()
-entry = tk.Entry(window, width = 20, textvariable = var_entry)
+entry = tk.Entry(window, width = 40, textvariable = var_entry)
 entry.insert("end","(在此输入股票代码)")
 entry.grid(row = 1, column = 2, columnspan = 4)
 
 # 下拉菜单
 set_listbox = set()
 var_listbox = tk.StringVar()
-listbox = tk.Listbox(window, height = 3, width = 20, listvariable = var_listbox)
+listbox = tk.Listbox(window, height = 3, width = 40, listvariable = var_listbox)
 listbox.insert("end", "暂无收藏股票序号     ")
 listbox.grid(row = 3, column = 2, columnspan = 4)
 
+# 插入一个空行显得好看一点
+empty = tk.Label(window, height = 1)
+empty.grid(row = 4)
+
 # 文本框
-text = tk.Text(window, height = 7, width = 30)
+text = tk.Text(window, height = 7, width = 40)
 var_text = tk.StringVar()
 text.insert("end",r'输入股票代码并选择交易所后点击"查询"按钮开始查询')
+text.configure(state = 'disabled')
 text.grid(row = 5, rowspan = 2, column = 2, columnspan = 4)
 
 # 选择项
@@ -78,6 +84,7 @@ radio_sz.grid(row = 9, column = 1)
 # "查询"按钮
 button_check = tk.Button(window, text = "查询", width = 10, height = 2)
 button_check.grid(row = 8, rowspan = 2,column = 3)
+button_check.bind(lambda : f.check_stock(stats, text, var_text, stock_exchange_code, entry, var_entry))
 
 # "收藏"按钮
 button_star = tk.Button(window, text = "收藏", width = 10, height = 2)
--- function.py
+++ function.py
@@ -12,4 +12,16 @@ def return_string(stock):
         s += "\n"
     return s
 
+def check_stock(stats, text, var_text, stock_exchange_code, entry, var_entry):
+    """
+    响应按钮"check"的事件
+    不直接调用stockScrapy,而是在stats中间接构造
+    stats:描述主程序状态,用所输入的信息进行初始化,以便于构建当前的股票信息
+    text: 将结果传入到text中显示给用户
+    var_text: 传入text的VarString对象,**为了程序的统一,从stats获取**
+    stock_exchange_code:直接获取app.py中的变量(交易所代码)
+    var_entry:从entry中获取信息并保存,提供以构造stockScrapy对象
+    """
+    pass
+
 
